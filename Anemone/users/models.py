b87795654105a68ee633b309263b68942b805746
Lenix Carter <lenix.carter@emory.edu>
2022-10-03 23:09:06 -0400

    Roughed out core database fmodels

--- Anemone/users/models.py
+++ Anemone/users/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(
+            User,
+            on_delete=models.CASCADE,      # On delete of user, profile is deleted
+            primary_key = True,          
+    )
+    household_code = models.CharField(max_length=8, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+class Message(models.Model):
+    sender = models.ForeignKey(
+            Profile,
+            on_delete=models.CASCADE
+    )
+    message_body = models.TextField()
+    datetime = models.DateTimeField()
+
+class Event(models.Model):
+    title = models.CharField(max_length=30)
+    body = models.TextField()
+    datetime = models.DateTimeField()
+
+class Chore(models.Model):
+    title = models.CharField(max_length=30)
+    body = models.TextField()
+    due_date = models.DateTimeField()
+    points = models.IntegerField()
+
+class Household(models.Model):
+    profiles = models.ManyToManyField(Profile)
+    messages = models.ManyToManyField(Message)
+    events = models.ManyToManyField(Event)
+    chores = models.ManyToManyField(Chore)
+
