71a3028f177fe10f8d3d14f627984b156b7e00f1
Mia703 <bookworm7572@gmail.com>
2022-10-27 09:41:05 -0400

    Attempted drag-and-drop (amya)
    - Very much ATTEMPTED the drag-and-drop function on task-board.html

--- amya-react/static-pages/actions/task-board.js
+++ amya-react/static-pages/actions/task-board.js
@@ -1,4 +1,24 @@
 // $(document)ready ... = prevents any jQuery code from running before the document finishes loading
 $(document).ready(function () {
-	
+
+
+
+	// while holding the mouse down on a task...
+	$(".a-task").mousedown(function () { 
+		$(this).draggable({
+			containment: ".task-column-container",
+			snap: '.column',
+			cursor: 'move'
+		});
+	});
+
+	// when I un-hold the task...
+	$(".a-task").mouseup(function () { 
+		
+	});
+
+	// can sort the tasks
+	$(".column").sortable ({
+		cursor: 'move'
+	});
 });
