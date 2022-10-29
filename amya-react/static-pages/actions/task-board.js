d26818557e302fff429146bc119d72b79207b734
Mia703 <bookworm7572@gmail.com>
2022-10-29 12:09:28 -0400

    Working on bid form
    - Finished task form
    - Started jQuery function for bidding
    - Created folder 'components' with reusable html

--- amya-react/static-pages/actions/task-board.js
+++ amya-react/static-pages/actions/task-board.js
@@ -1,12 +1,33 @@
 // TODO: make it so that you can only move cards on desktop
 $(document).ready(function () {
 
+	/* =================== CHECK FOR UNCLAIMED TASKS =================== */
+	
+	$(".a-task").click(
+		function (e) { 
+			e.preventDefault();
+			if ( $(this).hasClass("unclaimed") ) {
+				alert("unclaimed")
+				// add the bid button
+			}
+			else {
+				alert ("claimed")
+				// show the span and points
+			}
+		}
+	);
+
+	// on click bid button show bid form
+
+
+	/* =================== TASK CARD MOVE FUNCTIONS =================== */
 	// make is so I can move tasks around on the page
 	$(".column").sortable ({
 		cursor: 'move',
 		connectWith: '.column'
 	});
 
+	/* =================== SHOW AND HIDE TASK FORM =================== */
 	// make the Add Task button show the form (= slide in)
 	$(".right-buttons-container .button.right").click(function (e) { 
 		e.preventDefault();
@@ -23,33 +44,44 @@ $(document).ready(function () {
 			duration: '1000'
 		});
 	});
-
-	// make the submit button on the form close the form -- submit does some weird stuff
-	// $("#submit-btn").click(function (e) { 
-	// 	e.preventDefault();
-	// 	$("#form-container").hide();
-	// });
 	
+	/* =================== RETURN TODAY'S DATE =================== */
+	// make 'Created: XX/XX/XXXX' is always set to today's date (note the following code is JS)
 
+	var d = new Date(); // creates a date object
+	var month = d.getMonth()+1; // gets the month
+	var day = d.getDate(); // gets the day
+	// concats them together
+	var newOutput =  ((''+month).length<2 ? '0' : '') + month + '/' +
+	((''+day).length<2 ? '0' : '') + day  + '/' +
+	d.getFullYear();
+
+	// returns the variable. If the output is not working would show 'today's date here'
+	$("#todays-date").html(newOutput);
+	
+	/* =================== SHOW AND HIDE REPEAT INFORMATION ON FORM =================== */
 	// if "Repeats?" is checked then show the expaned info about repeat
 	$(".repeat-toggle-container #repeat_toggle").click(
 		function () {
 			if ( $(this).prop("checked") == true) {
 				// alert ("Checkbox is checked.")
-
-				// show the extra info regarding repeats
-				$(".repeat-expanded").show(); 
-
+				
 				// scroll on overflow
 				$(".task-form-container").css({"overflow-y":"scroll"});
+				
+				// show the extra info regarding repeats
+				$(".repeat-expanded").show(); 
 			}
 			else {
-				$(".repeat-expanded").hide();
-
 				// else, remove scroll bar
 				$(".task-form-container").css({"overflow-y":"visible"});
+				
+				// hide repeat extra information
+				$(".repeat-expanded").hide();
 			}
-		}
-	);
+	});
+
+	/* =================== CLOSE FORM ON SUBMIT =================== */
+	// make the submit button on the form close the form
 	
 });
