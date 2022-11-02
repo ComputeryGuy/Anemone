7e3f49cbca6c73ec65d2c66dfef955f432956517
121kj <121kj.kim@gmail.com>
2022-11-02 13:57:50 -0400

    adding leaderboard

---
+++ amya-react/static-pages/actions/leaderboard.js
@@ -0,0 +1,32 @@
+var ctx = document.getElementById('myChart').getContext('2d');
+// And for a doughnut chart
+var myDoughnutChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    datasets: [
+      {
+        data: [400, 600, 200],
+        backgroundColor: ['#BB86FC', '#6bafff', '#03DAC6',],
+      },
+    ],
+    labels: ['user1', 'user2', 'user3'],
+  },
+  options: {
+    legend: {
+      display: false,
+    },
+    scales: {
+      yAxes: [{
+        ticks: {
+          fontColor: "white",
+          beginAtZero: true
+        }
+      }],
+      xAxes: [{
+        ticks: {
+          fontColor: "white"
+        }
+      }]
+    }
+  }
+});
