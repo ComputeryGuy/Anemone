ff9c1b5b9475536f05989888a3350cf1e26e309c
121kj <121kj.kim@gmail.com>
2022-11-02 13:32:23 -0400

    login animation

--- amya-react/static-pages/actions/log-in.js
+++ amya-react/static-pages/actions/log-in.js
@@ -1,9 +1,34 @@
 
 // just type "jq" to access all jQuery snippets
 $(document).ready (function() {
-	// $("button").click(function() {
-	// 	$("p").hide();
-	// });
+	const signupBtn = document.querySelector('.signupBtn');
+	const signinBtn = document.querySelector('.signinBtn');
+	const btnBx = document.querySelector('.right-section');
+	const signinBx = document.querySelector('.sign-in-section');
+	const signupBx = document.querySelector('.sign-up-section');
+	const signupBtnBx = document.querySelector('.new-user-container');
+
+	function delay(milliseconds){
+		return new Promise(resolve => {
+			setTimeout(resolve, milliseconds);
+		});
+	}
+
+	signupBtn.onclick = async function() {
+		btnBx.classList.add('active');
+		signinBx.classList.add('none');
+		await delay(550);
+		signupBx.classList.remove('none');
+	}
+
+	signinBtn.onclick = async function() {
+		btnBx.classList.remove('active');
+		signupBx.classList.add('none');
+		signupBtnBx.classList.add('none');
+		await delay(550);
+		signinBx.classList.remove('none');
+		signupBtnBx.classList.remove('none');
+	}
 
 	
 });
